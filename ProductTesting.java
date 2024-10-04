83446ae35a5b08e6a282ad8093658b11d088340a
funmi <sannifunmilola26@gmail.com>
2024-10-04 07:27:34 +0100

    commit

---
+++ ProductTesting.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Testing {
+	@Test
+	public void testNegativeInput
+	//Given
+
+	//When
+
+	//Check
+
+
+}
