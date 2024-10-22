69f5ee6e6ef90b7356843265defa5712b0411c7d
funmi <sannifunmilola26@gmail.com>
2024-10-22 10:05:42 +0100

    update

--- MBTIQuestionAnswer.java
+++ MBTIQuestionAnswer.java
@@ -7,16 +7,18 @@ public class MBTIQuestionAnswer {
         Scanner input = new Scanner(System.in);
 
         System.out.print("What is your name? ");
-        String name = input.nextLine();
+        String name = input.nextLine().trim();
+        while(name.isEmpty()){
+            System.out.print("Name space is empty, try again");
+            name = input.nextLine().trim();
+        }
 
         int eCount = 0, iCount = 0;
         int sCount = 0, nCount = 0;
         int tCount = 0, fCount = 0;
         int jCount = 0, pCount = 0;
 
-        
-
-        
+            
         String[] userResponses = new String[20];
 
         System.out.println("\nHello, " + name + "! Kindly answer each question with 'A' or 'B' to determine your personality.\n");
@@ -61,8 +63,7 @@ public class MBTIQuestionAnswer {
             } 
             if (index % 4 == 1) { 
                 if (response.equals("A")) sCount++; 
-                else nCount++; 
-                
+                else nCount++;     
 
             }
             if (index % 4 == 2) { 
@@ -85,6 +86,7 @@ public class MBTIQuestionAnswer {
         mbti += (sCount >= nCount) ? "S" : "N";  
         mbti += (tCount >= fCount) ? "T" : "F";  
         mbti += (jCount >= pCount) ? "J" : "P";  
+        
 
 		
         System.out.println("\nHello " + name + ", you selected");
@@ -94,7 +96,14 @@ public class MBTIQuestionAnswer {
         printThinkingFeeling(userResponses, questions);
         printJudgingPerceptive(userResponses, questions);
         
-        System.out.println("\n" + name + ", your MBTI type is: " + mbti);
+        
+
+        String mbtiDescription = getMbtiDescription(mbti);
+        System.out.println(name + ", your MBTI type is: " + mbti);
+        System.out.println("Description: " + mbtiDescription);
+        
+   
+
 
     }
 
@@ -111,8 +120,9 @@ public class MBTIQuestionAnswer {
         if (userResponses[index].equals("A")) eCount++;
         else iCount++;
     }
-        System.out.println("Number of A selelected: " + eCount);
-        System.out.println("Number of B selelected: " + iCount + "\n");
+        System.out.println("Extrovert vs Introvert");
+        System.out.println("Number of A selected: " + eCount);
+        System.out.println("Number of B selected: " + iCount + "\n");
         
     }
 
@@ -128,6 +138,7 @@ public class MBTIQuestionAnswer {
         if (userResponses[index].equals("A")) sCount++;
         else nCount++;
     }
+        System.out.println("Sensing vs Intuition");
         System.out.println("Number of A selelected: " + sCount);
         System.out.println("Number of B selelected: " + nCount+ "\n");
       
@@ -145,6 +156,7 @@ public class MBTIQuestionAnswer {
         if (userResponses[index].equals("A")) tCount++;
         else fCount++;
     }
+        System.out.println("Thinking vs Feeling");
         System.out.println("Number of A selelected: " + tCount);
         System.out.println("Number of B selelected: " + fCount + "\n");
        
@@ -161,6 +173,7 @@ public class MBTIQuestionAnswer {
         if (userResponses[index].equals("A")) jCount++;
         else pCount++;
     }
+        System.out.println("Judging vs Perceptive");
         System.out.println("Number of A selelected: " + jCount);
         System.out.println("Number of B selelected: " + pCount + "\n");
         
@@ -180,4 +193,72 @@ public class MBTIQuestionAnswer {
             }
         }
     }
+
+
+    public static String getMbtiDescription(String mbti){
+        switch (mbti) {
+            case "INTJ": 
+		return "INTJ (Architect) is a personality type with the Introverted, Intuitive, Thinking, and Judging traits. These thoughtful tacticians love perfecting the details of life, applying creativity and rationality to everything they do. Their inner world is often a private, complex one.";
+
+            case "INFJ":
+               return "INFJ (Advocate) is a personality type with the Introverted, Intuitive, Feeling, and Judging traits. They tend to approach life with deep thoughtfulness and imagination. Their inner vision, personal values, and a quiet, principled version of humanism guide them in all things.";
+
+            case "ISTJ":
+               return "ISTJ (Logistician) is a personality type with the Introverted, Observant, Thinking, and Judging traits. These people tend to be reserved yet willful, with a rational outlook on life. They compose their actions carefully and carry them out with methodical purpose.";
+
+            case "ISTP":
+                return "ISTP (Virtuoso) is a personality type with the Introverted, Observant, Thinking, and Prospecting traits. They tend to have an individualistic mindset, pursuing goals without needing much external connection. They engage in life with inquisitiveness and personal skill, varying their approach as needed.";
+
+            case "INTP":
+                return "INTP (Logician) is a personality type with the Introverted, Intuitive, Thinking, and Prospecting traits. These flexible thinkers enjoy taking an unconventional approach to many aspects of life. They often seek out unlikely paths, mixing willingness to experiment with personal creativity.";
+
+            case "ENFJ":
+                return "ENFJ (Protagonist) is a personality type with the Extraverted, Intuitive, Feeling, and Judging traits. These warm, forthright types love helping others, and they tend to have strong ideas and values. They back their perspective with the creative energy to achieve their goals. When ENFJs care about someone, they want to help solve that person’s problems – sometimes at any cost. The good news is that many people are grateful for this assistance and advice. After all, there’s a reason that these personalities have a reputation for helping others improve their lives. People with the ENFJ personality type (Protagonists) feel called to serve a greater purpose in life. Thoughtful and idealistic, ENFJs strive to have a positive impact on other people and the world around them. These personalities rarely shy away from an opportunity to do the right thing, even when doing so is far from easy.";
+
+            case "ENFP":
+                return "ENFP (Campaigner) is a personality type with the Extraverted, Intuitive, Feeling, and Prospecting traits. These people tend to embrace big ideas and actions that reflect their sense of hope and goodwill toward others. Their vibrant energy can flow in many directions.";
+
+	    case "ENTJ":
+                return "ENTJ (Commander) is a personality type with the Extraverted, Intuitive, Thinking, and Judging traits. They are decisive people who love momentum and accomplishment. They gather information to construct their creative visions but rarely hesitate for long before acting on them.";
+
+            case "ENTP":
+                return "ENTP (Debater) is a personality type with the Extraverted, Intuitive, Thinking, and Prospecting traits. They tend to be bold and creative, deconstructing and rebuilding ideas with great mental agility. They pursue their goals vigorously despite any resistance they might encounter.";
+
+	    case "INFP":
+                return "INFP (Mediator) is a personality type with the Introverted, Intuitive, Feeling, and Prospecting traits. These rare personality types tend to be quiet, open-minded, and imaginative, and they apply a caring and creative approach to everything they do.";
+
+ 	    case "ISFJ":
+                return "ISFJ (Defender) is a personality type with the Introverted, Observant, Feeling, and Judging traits. These people tend to be warm and unassuming in their own steady way. They’re efficient and responsible, giving careful attention to practical details in their daily lives.";
+
+	    case "ESTJ":
+                return "ESTJ (Executive) is a personality type with the Extraverted, Observant, Thinking, and Judging traits. They possess great fortitude, emphatically following their own sensible judgment. They often serve as a stabilizing force among others, able to offer solid direction amid adversity.";
+
+
+	    case "ESFJ":
+                return "ESFJ (Consul) is a personality type with the Extraverted, Observant, Feeling, and Judging traits. They are attentive and people-focused, and they enjoy taking part in their social community. Their achievements are guided by decisive values, and they willingly offer guidance to others.";
+
+
+            case "ISFP":
+                return "ISFP (Adventurer) is a personality type with the Introverted, Observant, Feeling, and Prospecting traits. They tend to have open minds, approaching life, new experiences, and people with grounded warmth. Their ability to stay in the moment helps them uncover exciting potentials.";
+
+
+	    case "ESTP":
+                return "ESTP (Entrepreneur) is a personality type with the Extraverted, Observant, Thinking, and Prospecting traits. They tend to be energetic and action-oriented, deftly navigating whatever is in front of them. They love uncovering life’s opportunities, whether socializing with others or in more personal pursuits.";
+
+
+	    case "ESFP":
+                return "ESFP (Entertainer) is a personality type with the Extraverted, Observant, Feeling, and Prospecting traits. These people love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. They can be very social, often encouraging others into shared activities.";
+
+
+	    default:
+                return "A very special and interesting personality";
+
+
+        }
+
+
+
+    }
+
+
 }
